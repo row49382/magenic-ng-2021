e57b5ffcde80beda953427ae0e3c8b7c57fc0cce
Robert Wroblewski <r-wroblewski@bethel.edu>
2021-01-20 16:15:15 -0600

    testing different environment configs

--- project-one/src/app/app.component.ts
+++ project-one/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ export class AppComponent {
   title = 'project-one';
   name = 'Robert';
   section = 'learn something new';
+  appUrl = '';
+
+  constructor () {
+    this.appUrl = environment.apiUrl;
+  }
 }
--- project-one/src/environments/environment.qa.ts
+++ project-one/src/environments/environment.qa.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'prod url'
+  environmentName: 'qa',
+  apiUrl: 'qa url'
 };
